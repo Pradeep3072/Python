bd103895c7e0a709a9649df7bcc3ae330b20a67e
Pradeep3072 <pradeep3072003.d@gmail.com>
2024-05-06 11:05:35 -0500

    commit

--- Linear probing.py
+++ Linear probing.py
@@ -1,8 +1,29 @@
-n=[3,2,9,6,11,13,7,12]
-m=10
-c=[]
-for  i in range(len(n)):
-    k=n[i]
-    l=((2*k)+3)%m
+def hash_table(l, arr):
+    for i in l:
+        location=(2*i+3)%10
+        if arr[location]==-1:
+            arr[location]=i
+        else:
+            for j in range(len(arr)):
+                location=(i+j)%10
+                if arr[location] == -1:
+                    arr[location] = i
+                    break
+def display(arr):
+    for i in range(len(arr)):
+        if arr[i]==-1:
+            print(f"[{i}]=")
+        else:
+            print(f"[{i}]={arr[i]}")
+
+
+n = int(input("Enter no. of elements: "))
+l = list(map(int, input("Enter Elements: ").split()))
+arr = [-1] * 10
+hash_table(l, arr)
+display(arr)
+
+
+
 
 
