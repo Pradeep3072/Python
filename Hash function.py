9145645d4df166f258568bb6891ef2ea4fea4674
Pradeep3072 <pradeep3072003.d@gmail.com>
2024-05-06 09:52:20 -0500

    commited

--- Hash function.py
+++ Hash function.py
@@ -1,14 +1,25 @@
-a=[3,2,9,6,11,13,7,12]
+"""a=[3,2,9,6,11,13,7,12]
 m=10
 b=[]
-
+v=[]
 for i in range(len(a)):
     k=a[i]
     n=((2*k)+3)%10
     b.append(n)
-    for j in range(m):
+    for j in range(0,9):
         if b[j]==j:
-            b[j]=
+            v[j]=a[i]
+print(v)
+"""
 
-
-
+li=list(map(int,input().split()))
+m=int(input())
+v=[]
+for i in range(m):
+    v.append([i,[]])
+for i in li:
+    n=((2*i)+3)%10
+    if n<m:
+        v[n][1].append(i)
+for i in v:
+    print(i)
